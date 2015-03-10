a4de6588458e8aac448637f04c893b86fdf9a99a
Aaron Siegel <datadreamerlabs@gmail.com>
2015-03-09 21:45:59 -0700

    Functioning AJAX poll.

--- www/_js/site.js
+++ www/_js/site.js
@@ -1,4 +1,4 @@
-var pollanswer;
+var answer;
 
 /* smooth scrolling hack */
 $(document).ready(function(){
@@ -11,11 +11,35 @@ $(document).ready(function(){
 
 	    $('html, body').stop().animate({ 'scrollTop': $target.offset().top - $header.height() }, 900, 'swing');
 	});
+
 });
 
-function pollanswer(answer){
-	pollanswer = answer;
+function pollanswer(_answer){
+	// load the form and store the answer
+	answer = _answer;
 	$.get("_html/poll.html", function(data) {
-	     $("#poll").replaceWith(data);
+		$("#poll").replaceWith(data);
+
+		// set submit listener when form has been loaded
+		$("#watsonpollform").submit(function (e) {
+			e.preventDefault();
+			var age = $("#age").val(); 
+			var gender = $("#gender").val();
+			var party = $("#party").val();
+			var zipcode = $("#zipcode").val();
+			var reason = $("#reason").val();
+			var dataString = 'answer='+answer+'&age='+age+'&gender='+gender+'&party='+party+'&zipcode='+zipcode+'&reason='+reason;
+			console.log(dataString);
+			$.ajax({
+				type:'POST',
+				data:dataString,
+				url:'_php/pollsubmit.php',
+				success:function(data) {
+					//console.log(data);
+					$("#watsonpollform").replaceWith(data);
+				}
+			});
+		});
+
 	});
 }
---
+++ www/_php/pollsubmit.php
@@ -0,0 +1,32 @@
+<?php
+
+	$db = mysql_connect("localhost", "asiegel_web", "buttslol!") or die("Failed to connect to server.");
+	mysql_select_db("asiegel_watson") or die("Failed to select database.");
+
+	// check ip address to see if this person voted already
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$result = mysql_query("SELECT * FROM poll WHERE ip='{$ip}'");
+	if(mysql_num_rows($result) > 0){
+		echo "<div class='pollinstructions'>I'm sorry but a vote with your IP address has already been recorded in the database.</div>";
+	} else {
+
+		$answer = $_POST['answer'];
+		$age = $_POST['age'];
+		$gender = $_POST['gender'];
+		$party = $_POST['party'];
+		$zipcode = $_POST['zipcode'];
+		$reason = $_POST['reason'];
+
+		// TODO: check CAPTCHA input
+
+		$insert = "INSERT INTO poll (vote, gender, party, age, zipcode, reason, ip) values ('$answer', '$gender', '$party', '$age', '$zipcode', '$reason', '$ip')";
+		if(mysql_query($insert)) {
+			echo "<div class='pollinstructions'>Your vote has been recorded! Here are the current polls:</div>";
+			// TODO: echo results to user
+		} else {
+			echo mysql_error();
+			echo "<div class='pollinstructions'>I'm sorry but there was an error recording your vote. Please try again later.</div>";
+		}
+	}
+
+?>
